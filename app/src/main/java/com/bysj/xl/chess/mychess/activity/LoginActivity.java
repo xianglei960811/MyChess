abb15e449cf6f31a37493694324bf9f485f50e93
xianglei <1634409949@qq.com>
2018-09-21 11:57:32 +0800

    login界面

--- app/src/main/java/com/bysj/xl/chess/mychess/activity/LoginActivity.java
+++ app/src/main/java/com/bysj/xl/chess/mychess/activity/LoginActivity.java
@@ -1,8 +1,15 @@
 package com.bysj.xl.chess.mychess.activity;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bysj.xl.chess.mychess.Base.BaseActivity;
@@ -11,19 +18,60 @@ import com.bysj.xl.chess.mychess.WebSocketClient.MessageEvent.WebSocketErrorEven
 import com.bysj.xl.chess.mychess.WebSocketClient.Service.MyService;
 import com.bysj.xl.chess.mychess.WebSocketClient.Service.WebSocketSerice;
 import com.bysj.xl.chess.mychess.entity.CommonResponse;
+import com.bysj.xl.chess.mychess.entity.ReSizeDrawable.ReSizeDrawable;
+import com.bysj.xl.chess.mychess.entity.getScreenSize.ScreenSizeUtils;
 
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class LoginActivity extends BaseActivity {
-    @BindView(R.id.login_message)
-    TextView login_message;
-    @BindView(R.id.login_close)
-    Button login_close;
-
+    @BindView(R.id.login_user_et)
+    TextView login_user_et;
+    @BindView(R.id.log_user_ima)
+    ImageView log_user_ima;
+    @BindView(R.id.log_pass_ima)
+    ImageView log_pass_ima;
+    @BindView(R.id.login_pass_et)
+    EditText log_pass_et;
+    @BindView(R.id.log_log_ima)
+    ImageView log_log_ima;
+    @BindView(R.id.log_signIn_bt)
+    Button log_sigIn_bt;
+    @BindView(R.id.log_signUP_bt)
+    Button log_signUP_bt;
     @Override
     protected void initView() {
+        initImag();
+        int width = ScreenSizeUtils.getInstance(this).getScreenWidth() / 2;
+        int height = ScreenSizeUtils.getInstance(this).getScreenHeight() / 16;
+//        log_user_ll.setLayoutParams(new LinearLayout.LayoutParams(width,height));
+        login_user_et.setWidth(width);
+        login_user_et.setHeight(height);
+        log_pass_et.setWidth(width);
+        login_user_et.setHeight(height);
+        log_signUP_bt.setWidth(width/3*2);
+        log_signUP_bt.setHeight(height);
+        log_sigIn_bt.setWidth(width/3*2);
+        log_sigIn_bt.setHeight(height);
+
+
+    }
+
+    private void initImag() {
+        //todo 加载vector图标
+        Drawable drawable = ContextCompat.getDrawable(this, R.drawable.ic_user);
+        drawable = ReSizeDrawable.revector(this, (BitmapDrawable) drawable);
+        log_user_ima.setImageDrawable(drawable);
+
+        Drawable drawable1 = ContextCompat.getDrawable(this, R.drawable.ic_pass);
+        drawable1 = ReSizeDrawable.revector(this, (BitmapDrawable) drawable1);
+        log_pass_ima.setImageDrawable(drawable1);
+
+        Drawable drawable2 = ContextCompat.getDrawable(this, R.drawable.sigin);
+        drawable2 = ReSizeDrawable.relog(this, (BitmapDrawable) drawable2);
+        log_log_ima.setImageDrawable(drawable2);
+
 
     }
 
@@ -55,8 +103,4 @@ public class LoginActivity extends BaseActivity {
         return WebSocketSerice.class;
     }
 
-    @OnClick(R.id.login_close)
-    public void onCloseClick() {
-        senMsg("客户端1");
-    }
 }
--- app/src/main/java/com/bysj/xl/chess/mychess/entity/ReSizeDrawable/ReSizeDrawable.java
+++ app/src/main/java/com/bysj/xl/chess/mychess/entity/ReSizeDrawable/ReSizeDrawable.java
@@ -23,16 +23,16 @@ public class ReSizeDrawable {
         int bitmapWidth = bitmap.getWidth();
         int bitmapHeight = bitmap.getHeight();
 
-        float num = (float)bitmapWidth/((float) mScreenWidth);
-        if (num <1) {//图片比屏幕小，进行放大
-            float numb = ((float) (mScreenWidth / 3)/(float) bitmapWidth ) + 1.0f;
+        float num = (float) bitmapWidth / ((float) mScreenWidth);
+        if (num < 1) {//图片比屏幕小，进行放大
+            float numb = ((float) (mScreenWidth / 3) / (float) bitmapWidth) + 1.0f;
             Matrix matrix = new Matrix();
             matrix.postScale(num, numb);
             Log.d("ReSize", "reSize:is big--------------------> ");
             Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
             BitmapDrawable reSizeDrawble = new BitmapDrawable(context.getResources(), reSizeBitmap);
             return reSizeDrawble;
-        } else if (num >1) {//图片比屏幕宽度大，进行缩放
+        } else if (num > 1) {//图片比屏幕宽度大，进行缩放
             Matrix matrix = new Matrix();
             matrix.postScale(num, num);
             Log.d("ReSize", "reSize:is small--------------------> ");
@@ -49,6 +49,7 @@ public class ReSizeDrawable {
 
     /**
      * 对棋子进行缩放
+     *
      * @param context
      * @param drawable
      * @return
@@ -59,27 +60,136 @@ public class ReSizeDrawable {
         int bitmapWidth = bitmap.getWidth();
         int bitmapHeight = bitmap.getHeight();
 
-        float num = (float)bitmapWidth/((float) mScreenWidth/9/2);
-        if (num <1) {//图片比屏幕小，进行放大
-            float numb = ((float) (mScreenWidth / 9/6)/(float) bitmapWidth ) + 1.0f;
+        float num = (float) bitmapWidth / ((float) mScreenWidth / 9 / 2);
+        if (num < 1) {//图片比屏幕小，进行放大
+            float numb = ((float) (mScreenWidth / 9 / 6) / (float) bitmapWidth) + 1.0f;
             Matrix matrix = new Matrix();
             matrix.postScale(num, numb);
-            Log.d("ReSize", "reSize:is big--------------------> ");
+            Log.d("ReSize", "reSizePieces:is big--------------------> ");
             Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
             BitmapDrawable reSizeDrawble = new BitmapDrawable(context.getResources(), reSizeBitmap);
             return reSizeDrawble;
-        } else if (num >1) {//图片比屏幕宽度大，进行缩放
+        } else if (num > 1) {//图片比屏幕宽度大，进行缩放
             Matrix matrix = new Matrix();
             matrix.postScale(num, num);
-            Log.d("ReSize", "reSize:is small--------------------> ");
+            Log.d("ReSize", "reSizePieces:is small--------------------> ");
 //            Log.e("sss", "reSize: "+mScreenWidth+":"+bitmapWidth+":"+bitmapHeight+":"+matrix.toString() );
             Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
             BitmapDrawable reSizeDrawable = new BitmapDrawable(context.getResources(), reSizeBitmap);
             return reSizeDrawable;
         } else {
-            Log.d("ReSize", "reSize:is same--------------------> ");
+            Log.d("ReSize", "reSizePieces:is same--------------------> ");
             return drawable;
         }
 
+
+    }
+
+    /**
+     * 对vector图标缩放
+     *
+     * @param context
+     * @param drawable
+     * @return
+     */
+    public static BitmapDrawable revector(Context context, BitmapDrawable drawable) {
+        int mScreenWidth = ScreenSizeUtils.getInstance(context).getScreenWidth();
+        Bitmap bitmap = drawable.getBitmap();
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+
+        float num = (float) bitmapWidth / ((float) mScreenWidth / 11);
+        if (num < 1) {//图片比屏幕小，进行放大
+            float numb = ((float) (mScreenWidth / 11) / (float) bitmapWidth) + 1.0f;
+            Matrix matrix = new Matrix();
+            matrix.postScale(num, numb);
+            Log.d("ReSize", "revector:is big--------------------> ");
+            Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
+            BitmapDrawable reSizeDrawble = new BitmapDrawable(context.getResources(), reSizeBitmap);
+            return reSizeDrawble;
+        } else if (num > 1) {//图片比屏幕宽度大，进行缩放
+            Matrix matrix = new Matrix();
+            matrix.postScale(num, num);
+            Log.d("ReSize", "revector:is small--------------------> ");
+//            Log.e("sss", "reSize: "+mScreenWidth+":"+bitmapWidth+":"+bitmapHeight+":"+matrix.toString() );
+            Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
+            BitmapDrawable reSizeDrawable = new BitmapDrawable(context.getResources(), reSizeBitmap);
+            return reSizeDrawable;
+        } else {
+            Log.d("ReSize", "revector:is same--------------------> ");
+            return drawable;
+        }
+    }
+
+    /**
+     * 对log图片
+     *
+     * @param context
+     * @param drawable
+     * @return
+     */
+    public static BitmapDrawable relog(Context context, BitmapDrawable drawable) {
+        int mScreenWidth = ScreenSizeUtils.getInstance(context).getScreenWidth();
+        Bitmap bitmap = drawable.getBitmap();
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+
+        float num = (float) bitmapWidth / ((float) mScreenWidth / 3);
+        if (num < 1) {//图片比屏幕小，进行放大
+            float numb = ((float) (mScreenWidth / 3) / (float) bitmapWidth) + 1.0f;
+            Matrix matrix = new Matrix();
+            matrix.postScale(num, numb);
+            Log.d("ReSize", "relog:is big--------------------> ");
+            Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
+            BitmapDrawable reSizeDrawble = new BitmapDrawable(context.getResources(), reSizeBitmap);
+            return reSizeDrawble;
+        } else if (num > 1) {//图片比屏幕宽度大，进行缩放
+            Matrix matrix = new Matrix();
+            matrix.postScale(num, num);
+            Log.d("ReSize", "relog:is small--------------------> ");
+//            Log.e("sss", "reSize: "+mScreenWidth+":"+bitmapWidth+":"+bitmapHeight+":"+matrix.toString() );
+            Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
+            BitmapDrawable reSizeDrawable = new BitmapDrawable(context.getResources(), reSizeBitmap);
+            return reSizeDrawable;
+        } else {
+            Log.d("ReSize", "relog:is same--------------------> ");
+            return drawable;
+        }
+    }
+
+    /**
+     * 对denglu图片
+     *
+     * @param context
+     * @param drawable
+     * @return
+     */
+    public static BitmapDrawable reLogBt(Context context, BitmapDrawable drawable) {
+        int mScreenWidth = ScreenSizeUtils.getInstance(context).getScreenWidth();
+        Bitmap bitmap = drawable.getBitmap();
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+
+        float num = (float) bitmapWidth / ((float) mScreenWidth / 5*4);
+        if (num < 1) {//图片比屏幕小，进行放大
+            float numb = ((float) (mScreenWidth / 3 ) / (float) bitmapWidth) + 1.0f;
+            Matrix matrix = new Matrix();
+            matrix.postScale(num, numb);
+            Log.d("ReSize", "reLogBt:is big--------------------> ");
+            Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
+            BitmapDrawable reSizeDrawble = new BitmapDrawable(context.getResources(), reSizeBitmap);
+            return reSizeDrawble;
+        } else if (num > 1) {//图片比屏幕宽度大，进行缩放
+            Matrix matrix = new Matrix();
+            matrix.postScale(num, num);
+            Log.d("ReSize", "reLogBt:is small--------------------> ");
+//            Log.e("sss", "reSize: "+mScreenWidth+":"+bitmapWidth+":"+bitmapHeight+":"+matrix.toString() );
+            Bitmap reSizeBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmapWidth, bitmapHeight, matrix, true);
+            BitmapDrawable reSizeDrawable = new BitmapDrawable(context.getResources(), reSizeBitmap);
+            return reSizeDrawable;
+        } else {
+            Log.d("ReSize", "reLogBt:is same--------------------> ");
+            return drawable;
+        }
     }
 }
